b6ac8a9393ef8750a6f9d6e31f52a9ed9bdfd22b
zhengyang2 <67302728+zhengyang2@users.noreply.github.com>
2024-09-11 15:49:22 +0800

    add job role page

--- app.py
+++ app.py
@@ -11,5 +11,10 @@ def Home():
 def Industries():
     return render_template('industries.html')
 
+
+@app.route('/job_roles')
+def Job_roles():
+    return render_template('job_roles.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
