ed1e4f5a988383063bbe9708eb8eae996584a383
ryan wong <wong.ryan5091@gmail.com>
2024-09-21 18:05:19 +0800

    refractor url course grabber

--- course_url_crawler.py
+++ course_url_crawler.py
@@ -1,38 +1,35 @@
 import requests
 
-def search_courses(platform, search_terms):
+def search_courses(search_terms):
     all_courses = []  # List to store all course results
-    if platform == 'coursera':
-        url = "https://www.coursera.org/api/courses.v1"
-        for term in search_terms:
-            params = {
-                "q": "search",
-                "query": term,
-                "includes": "instructor_ids",
-                "limit": 5
-            }
-            headers = {
-                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
-                'Referer': 'https://www.coursera.org/'
-            }
-            response = requests.get(url, params=params, headers=headers)
-            if response.status_code == 200:
-                data = response.json()
-                courses = data['elements']
-                if courses:
-                    # Limit to the top 3 courses
-                    for idx, course in enumerate(courses[:3], 1):
-                        course_info = f"{idx}. {course['name']} - https://www.coursera.org/learn/{course['slug']}"
-                        all_courses.append(course_info)
-                else:
-                    continue
+    url = "https://www.coursera.org/api/courses.v1"
+    for term in search_terms:
+        params = {
+            "q": "search",
+            "query": term,
+            "includes": "instructor_ids",
+            "limit": 5
+        }
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
+            'Referer': 'https://www.coursera.org/'
+        }
+        response = requests.get(url, params=params, headers=headers)
+        if response.status_code == 200:
+            data = response.json()
+            courses = data['elements']
+            if courses:
+                # Limit to the top 3 courses
+                for idx, course in enumerate(courses[:3], 1):
+                    course_info = f"{course['name']} - https://www.coursera.org/learn/{course['slug']}"
+                    all_courses.append(course_info)
             else:
                 continue
 
     # Print all collected courses
     for course in all_courses:
         print(course)
-
+    
 if __name__ == '__main__':
     search_terms = ['java', 'UI', 'python programming']
-    search_courses('coursera', search_terms)
+    search_courses(search_terms)
