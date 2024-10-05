fd8c0e386d89426ec7b429e129ea5dd45ae08430
sabihahamir12 <sabihahamir12@gmail.com>
2024-10-05 23:42:37 +0800

    put description of ml file

--- Text_Classification-KMeans.py
+++ Text_Classification-KMeans.py
@@ -1,8 +1,34 @@
+'''
+Author: Sabihah Amirudeen
+Project: SkillGauge - Job Data Clustering
+
+Description:
+This Python script performs text-based clustering on job data using KMeans. 
+The script first combines relevant text features from the job dataset (such as 'Job Title', 'Responsibilities', and 'Skills')
+into a single feature for analysis. It then uses TF-IDF (Term Frequency-Inverse Document Frequency) to convert the 
+textual data into numerical representations. 
+
+KMeans clustering is applied to group similar jobs into clusters, which are 
+labeled as 'Predicted Industry'. The resulting clustered jobs are saved  in a CSV file. This script helps to identify patterns and groupings in the 
+job market data, aiding fresh graduates and professionals in understanding trends 
+in job roles and industries.
+
+The words will be grouped among 30 clusters.
+
+A short snippet of the result:
+output:                         Job Title  Predicted Industry
+0                       Architect                  15
+1     Customer Support Specialist                  18
+2                   Event Planner                  20
+3               Software Engineer                  16
+4                   Data Engineer                   6
+
+'''
+
+
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.cluster import KMeans
-
-
 
 # Load your data
 data = pd.read_csv('Datasets\sg_job_data-Cleaned.csv')
@@ -27,13 +53,13 @@ data['Predicted Industry'] = kmeans.labels_
 # Group jobs by predicted clusters (Predicted Industry)
 grouped_jobs = data.groupby('Predicted Industry')['Job Title'].apply(list)
 
-# Save the clusters and jobs within each cluster to a text file
-with open('Datasets/clustered_jobs.txt', 'w') as f:
-    for cluster, jobs in grouped_jobs.items():
-        f.write(f"Cluster {cluster}:\n")
-        for job in jobs:
-            f.write(f"- {job}\n")
-        f.write("\n")
+# # Save the clusters and jobs within each cluster to a text file
+# with open('Datasets/clustered_jobs.txt', 'w') as f:
+#     for cluster, jobs in grouped_jobs.items():
+#         f.write(f"Cluster {cluster}:\n")
+#         for job in jobs:
+#             f.write(f"- {job}\n")
+#         f.write("\n")
 
 
 # Print the jobs grouped by clusters to inspect
